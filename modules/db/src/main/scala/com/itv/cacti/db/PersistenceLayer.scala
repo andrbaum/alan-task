57469a381d8c9ac38de1206060acdac6cfd01c94
Alan Ciccotelli <alanciccotelli9@gmail.com>
2025-05-22 16:00:42 +0100

    feat: added tests

--- modules/db/src/main/scala/com/itv/cacti/db/PersistenceLayer.scala
+++ modules/db/src/main/scala/com/itv/cacti/db/PersistenceLayer.scala
@@ -7,6 +7,7 @@ import java.util.UUID
 import com.itv.cacti.core.OperationStatus
 import com.itv.cacti.core.Pokemon
 import com.itv.cacti.core.PokemonType
+import cats.effect.IO
 
 trait PersistenceLayer[F[_]] {
 
